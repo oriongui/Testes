722a240ec2ca7b0801eb0487c63e7dbf2f32d884
Orion Guimarães <orion.gui@hotmail.com>
2022-10-31 16:53:07 -0300

    Criada página index

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	class Musica {
+		public $nome;
+		public $cantor;
+		public function __construct($nome, $cantor){
+			$this->nome = $nome;
+			$this->cantor = $cantor;
+		}
+		public function qual(){
+			return "$this->nome, musica do cantor $this->cantor";
+			
+		}
+		
+	}
+	
+	
+	
+	
+?>
+	<!--
+	<html>
+		<body>
+			<form  method="POST" action="index.php" >
+				<label>Música:</label>
+				<input type='text' name='musica' id='musica'> <br>
+				<label>Cantor:</label>
+				<input type='text' name='cantor' id='cantor'><br>
+				<input type='submit' >
+			</form>
+		</body>
+	
+	
+	</html>
+	-->
+	<html>
+		
+	</html>
+<?php
+	
+	#Usar formulário e funções da classe Musica
+	/*if($_POST != NULL){
+		$musica = new Musica($_POST['musica'], $_POST['cantor']);
+	
+		echo $musica->qual();
+	}*/
+	
+	
+?>
